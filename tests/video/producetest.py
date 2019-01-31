e48fc7a9f8c562ae84ce1ec6bff26c99b68c557b
Ubuntu <ubuntu@ip-10-0-0-11.us-west-2.compute.internal>
2019-01-31 02:49:42 +0000

    single preoducer tested

--- tests/video/producetest.py
+++ tests/video/producetest.py
@@ -2,16 +2,18 @@ import sys
 sys.path.append('../../')
 
 from src.producer.video_producer import StreamVideo
-
+from src.kafka.utils import clear_topic, set_topic
 
 if __name__ == "__main__":
     url = 'http://s3-us-west-2.amazonaws.com/dashcash/dataset/train-14/videos/c4dab4e2-6c4805a2.mov'
     topic = "video_test_org"
     partitions = 16
     use_cv2 = True
-    verbose = True
+    verbose = False
     pub_obj_key = "test"
     rr_distribute = False
+    clear_topic(topic)
+    set_topic(topic)
     producer = StreamVideo(url, topic, partitions,
                            use_cv2=use_cv2,
                            verbose=verbose,
