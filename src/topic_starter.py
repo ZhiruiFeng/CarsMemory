38ca46e1feff50953d4ea3dbd1fb54767d715d94
ZhiruiFeng <fihezro@gmail.com>
2019-02-07 18:56:16 -0800

    interesting scene for self driving cars

---
+++ src/topic_starter.py
@@ -0,0 +1,15 @@
+"""Only need to start once for the whole system"""
+from src.kafka.utils import set_topic
+from src.params import FRAME_PARTITIONS, URL_PARTITIONS, VALUE_PARTITIONS
+
+
+if __name__ == "__main__":
+    frame_topic = "org"
+    frame_partitions = FRAME_PARTITIONS
+    url_topic = "url"
+    url_partitions = URL_PARTITIONS
+    value_topic = "value"
+    value_partitions = VALUE_PARTITIONS
+    set_topic(frame_topic, partitions=frame_partitions)
+    set_topic(url_topic, partitions=url_partitions)
+    set_topic(value_topic, partitions=value_partitions)
